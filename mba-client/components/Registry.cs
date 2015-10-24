390bbdb7ad6c779be07111df039eb1299236b8f8
chasik <chasow@yandex.ru>
2015-10-24 15:55:21 +0300

    first commit for mba finance

---
+++ mba-client/components/Registry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpf.Docking;
+
+namespace mba_client.components
+{
+    class Registry
+    {
+        private static Registry instance;
+        public Registry (DockLayoutManager layoutManager)
+        {
+                 //       < dxdo:LayoutGroup >
+                 //< dxdo:DocumentGroup >
+                 //     < dxdo:DocumentPanel Caption = "Document" >
+                 //          < Grid />
+                 //      </ dxdo:DocumentPanel >
+                 //       < dxdo:DocumentPanel Caption = "Document" >
+                 //            < Grid />
+                 //        </ dxdo:DocumentPanel >
+                 //     </ dxdo:DocumentGroup >
+                 //  </ dxdo:LayoutGroup >
+        }
+
+        public static Registry GetInstance(DockLayoutManager layoutManager)
+        {
+            instance = instance ?? new Registry(layoutManager);
+            return instance;
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wcf-authentication/AuthNTLM.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wcf_authentication
+{
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AuthNTLM.svc or AuthNTLM.svc.cs at the Solution Explorer and start debugging.
+    public class AuthNTLM : IAuthNTLM
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
