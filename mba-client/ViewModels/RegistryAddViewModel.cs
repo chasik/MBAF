07b4365ac46d500d59d2d68ed227f36c11383f31
chasik <chasow@yandex.ru>
2015-11-02 18:19:01 +0300

    try use datatemplate

--- mba-client/ViewModels/RegistryAddViewModel.cs
+++ mba-client/ViewModels/RegistryAddViewModel.cs
@@ -4,17 +4,38 @@ using DevExpress.Xpf.Spreadsheet;
 using mba_client.components;
 using DevExpress.Spreadsheet;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace mba_client.ViewModels
 {
     public class RegistryAddViewModel : ViewModelBase
     {
-        private INavigationService NavigationService { get { return this.GetService<INavigationService>(); } }
-
+        private INavigationService NavigationService { get { return GetService<INavigationService>(); } }
+        public RegistryAddViewModel()
+        {
+            ShowProgressBar = "Collapsed";
+            WorkSheetsCount = "Результаты парсинга документа ...";
+            WorkSheetsInBook = new ObservableCollection<SheetInfo>();
+        }
         public string SourceFilePath
         {
             get { return GetProperty(() => SourceFilePath); }
             set { SetProperty(() => SourceFilePath, value); }
+        }
+        public string ShowProgressBar
+        {
+            get { return GetProperty(() => ShowProgressBar); }
+            set { SetProperty(() => ShowProgressBar, value); }
+        }
+        public string WorkSheetsCount
+        {
+            get { return GetProperty(() => WorkSheetsCount); }
+            set { SetProperty(() => WorkSheetsCount, value); }
+        }
+        public ObservableCollection<SheetInfo> WorkSheetsInBook
+        {
+            get { return GetProperty(() => WorkSheetsInBook); }
+            set { SetProperty(() => WorkSheetsInBook, value); }
         }
         public void DblClickExplorer(FileSystemItem focusedNode)
         {
@@ -23,35 +44,40 @@ namespace mba_client.ViewModels
                     || focusedNode.FullName.IndexOf(".xlsx", 0, StringComparison.InvariantCultureIgnoreCase) > -1
                )
             {
+                WorkSheetsInBook.Clear();
                 SourceFilePath = focusedNode.FullName;
             }
         }
 
         public void DocumentLoaded(object _spreadSheet)
         {
+            //ShowProgressBar = "Visible";
             if (_spreadSheet is SpreadsheetControl)
             {
                 IWorkbook WorkBook = (_spreadSheet as SpreadsheetControl).Document;
-
-                RangeInfo rangeInfo;
+                WorkSheetsCount = "Найдено листов: " + WorkBook.Worksheets.Count.ToString();
                 foreach (Worksheet ws in WorkBook.Worksheets)
                 {
                     Range usedRange = ws.GetUsedRange();
-                    rangeInfo = new RangeInfo(usedRange.RightColumnIndex - usedRange.LeftColumnIndex);
+
+                    SheetInfo sheetInfo = new SheetInfo(usedRange.RightColumnIndex - usedRange.LeftColumnIndex);
+                    sheetInfo.WorkSheetName = ws.Name;
                     for (int i = usedRange.TopRowIndex; i < usedRange.BottomRowIndex; i++)
                     {
-                        rangeInfo.AddNewRow();
+                        sheetInfo.AddNewRow();
                         for (int j = usedRange.LeftColumnIndex; j < usedRange.RightColumnIndex; j++)
-                            rangeInfo.CurrentRowInfo.AddCell(ws.Cells[j, i]);
+                            sheetInfo.CurrentRowInfo.AddCell(ws.Cells[i, j]);
 
-                        rangeInfo.GetStatisticForRow();
+                        sheetInfo.GetStatisticForRow();
                     }
+                    WorkSheetsInBook.Add(sheetInfo);
                 }
             }
+            //ShowProgressBar = "Collapsed";
         }
     }
 
-    internal class RowStruct
+    public class RowStruct
     {
         private int columnsCount;
         private int count;
@@ -64,12 +90,18 @@ namespace mba_client.ViewModels
         }
     }
 
-    class RangeInfo
+    public class SheetInfo : BindableBase
     {
         private List<RowInfo> rangeRows;
         private int columnsCount;
+        public string WorkSheetName
+        {
+            get { return GetProperty(() => WorkSheetName); }
+            set { SetProperty(() => WorkSheetName, value); }
+        }
         public RowInfo CurrentRowInfo { get; private set; }
-        public RangeInfo(int _columnsCount)
+
+        public SheetInfo(int _columnsCount)
         {
             rangeRows = new List<RowInfo>();
             columnsCount = _columnsCount;
@@ -81,12 +113,16 @@ namespace mba_client.ViewModels
         }
         internal void GetStatisticForRow()
         {
+            foreach (var cellType in CurrentRowInfo.cellTypesDictionary)
+            {
+
+            }
         }
     }
 
-    class RowInfo
+    public class RowInfo
     {
-        private Dictionary<CellValueType, RowStruct> cellTypesDictionary;
+        public Dictionary<CellValueType, RowStruct> cellTypesDictionary;
         private int columnsCount;
         public RowInfo(int _columnsCount)
         {
@@ -97,7 +133,7 @@ namespace mba_client.ViewModels
         {
             CellValueType cellType = cell.Value.Type;
             if (cellTypesDictionary.ContainsKey(cellType))
-                cellTypesDictionary[cellType].Count = 2;
+                cellTypesDictionary[cellType].Count += 1;
             else
                 cellTypesDictionary.Add(cellType, new RowStruct(columnsCount));
         }
