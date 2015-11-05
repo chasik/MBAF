9d36c564d1698cc225fc4d9f92742edf73dbfc98
Ivan Chasov <chasow@yandex.ru>
2015-11-05 22:22:11 +0300

    add mba-application

---
+++ mba-application/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Xpf.Core;
+
+namespace mba_application
+{
+    public partial class MainWindow : DXWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
