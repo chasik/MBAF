996944d402e01ca24668271a859811550c1890a7
chasik <chasow@yandex.ru>
2015-11-23 15:02:01 +0300

    add menu item for operator and project manager

---
+++ mba-application/ViewModels/Operator/OperatorWorkflowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+
+namespace mba_application.ViewModels.Operator
+{
+    [POCOViewModel]
+    public class OperatorWorkflowViewModel
+    {
+        protected OperatorWorkflowViewModel() { }
+
+        public static OperatorWorkflowViewModel Create()
+        {
+            return ViewModelSource.Create(() => new OperatorWorkflowViewModel());
+        }
+    }
+}
--- mba-model/Migrations/Configuration.cs
+++ mba-model/Migrations/Configuration.cs
@@ -41,35 +41,46 @@ namespace mba_model.Migrations
                 new Permission { Id = 8, ParentId = null, Name = "menu-admin", ScreenName = "Администрирование", Tooltip = "Раздел меню администрирование системы", Image = "menu-group-admin.png" },
 
                 new Permission { Id = 9, ParentId = 1, Name = "menu-processing-work", ScreenName = "Выборки",
-                                 Tooltip = "Рабочее пространство оператора", Image = "menu-item-processing-operator.png", CommandParam = ""
-                               },
+                                 Tooltip = "Рабочее пространство оператора", Image = "menu-item-processing-operator.png", CommandParam = "OperatorWorkflowView" },
                 new Permission { Id = 10, ParentId = 1, Name = "menu-processing-table", ScreenName = "Табели",
-                                 Tooltip = "Дополнительные табели для операторов", Image = "menu-item-processing-table.png", CommandParam = ""
-                               },
-                new Permission { Id = 11, ParentId = 8, Name = "menu-admin-users", ScreenName = "Пользователи",
-                                 Tooltip = "Установка разрешений для пользователей", Image = "menu-item-admin-users.png", CommandParam = "AdminUsersView"
-                               },
-                new Permission { Id = 12, ParentId = 8, Name = "menu-admin-aster", ScreenName = "Asterisk",
-                                 Tooltip = "Настройка и мониторинг Asterisk", Image = "menu-item-admin-aster.png", CommandParam = "AdminAsteriskView"
-                               },
-                new Permission { Id = 13, ParentId = 3, Name = "menu-import-registry", ScreenName = "Реестры",
-                                 Tooltip = "Импорт реестров", Image = "menu-item-import-registry.png", CommandParam = "RegistryAddView"
-                               },
-                new Permission { Id = 14, ParentId = 4, Name = "menu-analytics-dashboard", ScreenName = "Мониторинг",
-                                 Tooltip = "Текущая состояние колл-центра", Image = "menu-item-analytics-dashboard.png", CommandParam = ""
-                               },
-                new Permission { Id = 15, ParentId = 5, Name = "menu-report-reports", ScreenName = "Отчеты",
-                                 Tooltip = "Настройка отчетов", Image = "menu-item-report-reports.png", CommandParam = ""
-                               },
-                new Permission { Id = 16, ParentId = 2, Name = "menu-lead-tables", ScreenName = "Табели",
-                                 Tooltip = "Заполнение табелей", Image = "menu-item-lead-table.png", CommandParam = ""
-                               },
-                new Permission { Id = 17, ParentId = 6, Name = "menu-search-address", ScreenName = "Адресса",
-                                 Tooltip = "Поиск и проверка адресов", Image = "menu-item-search-address.png", CommandParam = ""
-                               },
-                new Permission { Id = 18, ParentId = 7, Name = "menu-leadership-dashboard", ScreenName = "Мониторинг",
-                                 Tooltip = "Мониторинг рабочего процесса", Image = "menu-item-leadership-dashboard.png", CommandParam = ""
-                               }
+                                 Tooltip = "Дополнительные табели для операторов", Image = "menu-item-processing-table.png", CommandParam = "" },
+                new Permission { Id = 11, ParentId = 1, Name = "menu-processing-task", ScreenName = "Задания",
+                                 Tooltip = "Дополнительные табели для операторов", Image = "menu-item-processing-task.png", CommandParam = "" },
+
+                new Permission { Id = 12, ParentId = 2, Name = "menu-lead-tables", ScreenName = "Табели",
+                                 Tooltip = "Заполнение табелей", Image = "menu-item-lead-table.png", CommandParam = "" },
+                new Permission { Id = 13, ParentId = 2, Name = "menu-lead-operators", ScreenName = "Выборки",
+                                 Tooltip = "Формирование заданий (выборок) для операторов", Image = "menu-item-lead-operators.png", CommandParam = "" },
+                new Permission { Id = 14, ParentId = 2, Name = "menu-lead-dialer", ScreenName = "Dialer",
+                                 Tooltip = "Задания для автоматического выполнения", Image = "menu-item-lead-dialer.png", CommandParam = "" },
+                new Permission { Id = 15, ParentId = 2, Name = "menu-lead-registry", ScreenName = "Реестры",
+                                 Tooltip = "Работа с реестрами по проекту", Image = "menu-item-lead-registry.png", CommandParam = "" },
+                new Permission { Id = 16, ParentId = 2, Name = "menu-lead-monitoring", ScreenName = "Мониторинг",
+                                 Tooltip = "Мониторинг активности операторов", Image = "menu-item-lead-monitoring.png", CommandParam = "" },
+
+                new Permission { Id = 17, ParentId = 3, Name = "menu-import-registry", ScreenName = "Реестры",
+                                 Tooltip = "Импорт реестров", Image = "menu-item-import-registry.png", CommandParam = "RegistryAddView" },
+
+                new Permission { Id = 18, ParentId = 4, Name = "menu-analytics-dashboard", ScreenName = "Мониторинг",
+                                 Tooltip = "Текущая состояние колл-центра", Image = "menu-item-analytics-dashboard.png", CommandParam = "" },
+
+                new Permission { Id = 19, ParentId = 5, Name = "menu-report-reports", ScreenName = "Отчеты",
+                                 Tooltip = "Настройка отчетов", Image = "menu-item-report-reports.png", CommandParam = "" },
+
+                new Permission { Id = 20, ParentId = 6, Name = "menu-search-address", ScreenName = "Адресса",
+                                 Tooltip = "Поиск и проверка адресов", Image = "menu-item-search-address.png", CommandParam = "" },
+                new Permission { Id = 21, ParentId = 6, Name = "menu-search-phone", ScreenName = "Телефоны",
+                                 Tooltip = "Поиск и проверка телефонов", Image = "menu-item-search-phone.png", CommandParam = "" },
+                new Permission { Id = 22, ParentId = 6, Name = "menu-search-opp", ScreenName = "ОПП",
+                                 Tooltip = "Отдел предварительного поиска", Image = "menu-item-search-opp.png", CommandParam = "" },
+
+                new Permission { Id = 23, ParentId = 7, Name = "menu-leadership-dashboard", ScreenName = "Мониторинг",
+                                 Tooltip = "Мониторинг рабочего процесса", Image = "menu-item-leadership-dashboard.png", CommandParam = "" },
+
+                new Permission { Id = 24, ParentId = 8, Name = "menu-admin-users", ScreenName = "Пользователи",
+                                 Tooltip = "Установка разрешений для пользователей", Image = "menu-item-admin-users.png", CommandParam = "AdminUsersView" },
+                new Permission { Id = 25, ParentId = 8, Name = "menu-admin-aster", ScreenName = "Asterisk",
+                                 Tooltip = "Настройка и мониторинг Asterisk", Image = "menu-item-admin-aster.png", CommandParam = "AdminAsteriskView" }
                 );
 
             context.Actions.AddOrUpdate(
@@ -123,7 +134,8 @@ namespace mba_model.Migrations
                 new ImportType { Id = 1, Name = "Импорт реестров" },
                 new ImportType { Id = 2, Name = "Платежи по реестрам" },
                 new ImportType { Id = 3, Name = "Корректировки по реестрам" },
-                new ImportType { Id = 4, Name = "Дополнительные контакты" }
+                new ImportType { Id = 4, Name = "Дополнительные контакты" },
+                new ImportType { Id = 5, Name = "Специальные предложения банка" }
                 );
 
             context.SaveChanges();
