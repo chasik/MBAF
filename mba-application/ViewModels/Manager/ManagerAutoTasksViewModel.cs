2a9463027381a3c87e557a36cadfb346e528b315
chasik <chasow@yandex.ru>
2015-11-23 18:07:26 +0300

    workflow for project manager

---
+++ mba-application/ViewModels/Manager/ManagerAutoTasksViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+
+namespace mba_application.ViewModels.Manager
+{
+    [POCOViewModel]
+    public class ManagerAutoTasksViewModel
+    {
+        protected ManagerAutoTasksViewModel() { }
+
+        public static ManagerAutoTasksViewModel Create()
+        {
+            return ViewModelSource.Create(() => new ManagerAutoTasksViewModel());
+        }
+    }
+}
---
+++ mba-application/ViewModels/Manager/ManagerTasksViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+
+namespace mba_application.ViewModels.Manager
+{
+    [POCOViewModel]
+    public class ManagerTasksViewModel
+    {
+        protected ManagerTasksViewModel() { }
+
+        public static ManagerTasksViewModel Create()
+        {
+            return ViewModelSource.Create(() => new ManagerTasksViewModel());
+        }
+    }
+}
